5c210c9dcb11c5dff7bc966e4643dff6844fc1c5
César Ramos di benedetto <24.damt.cesaralejandroramos@fundacionmontessori.com>
2024-11-27 16:43:29 +0100

    prueba y personaje adicional

--- src/historia_terror/Historia_Cannon.java
+++ src/historia_terror/Historia_Cannon.java
@@ -4,7 +4,8 @@ public class Historia_Cannon {
 
 	public static void main(String[] args) {
 
-		System.out.println("mi nombre es toshi y esta es mi historia....");
+		System.out.println("En el años 1988 en una familia de origen inglesa conformada por el padre, jotaru joestar y su hijo jose joestar");
+		System.out.println("...");
 
 	}
 
---
+++ src/historia_terror/Personaje_C.java
@@ -0,0 +1,16 @@
+package historia_terror;
+
+public class Personaje_C {
+
+	
+	public static void main(String[] args) {
+
+		System.out.println("Nombre: Jonny joestar");
+		System.out.println("Edad: 60 años");
+		System.out.println("Soy decendiente de una familia ingles con una extraña marca de nacimiento"
+				+"Tengo una estatura de: 195 cm con fisico atletico para mi edad"
+				+"Tengo un hijo y la sospecha de un nieto perdido");
+
+	}
+	
+}
