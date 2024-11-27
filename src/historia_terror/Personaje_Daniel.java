f314a27c375d821d6217551e43bde1cb3355368e
dr <24.damt.danielrevuelto@fundacionmontessori.com>
2024-11-27 16:18:16 +0100

    esta es la historia de mi vida

--- src/historia_terror/Personaje_Daniel.java
+++ src/historia_terror/Personaje_Daniel.java
@@ -3,7 +3,11 @@ package historia_terror;
 public class Personaje_Daniel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Nombre: Jotaru Joestar");
+		System.out.println("Edad: 51");
+		System.out.println("Tengo una rara marca de nacimiento en forma de estrella en la parte trasera de mi hombre izquierdo "
+				+ "y un buen físico para mi edad,"
+				+ " creo que tengo un hijo en alguna parte de este mundo");
 
 	}
 
