4e431afcd01966fa6996364f2def25d5acd346fe
martin <22.sis.martinlopez@fundacionmontessori.com>
2024-11-27 16:42:05 +0100

    a

--- src/historia_terror/Personaje_Martin.java
+++ src/historia_terror/Personaje_Martin.java
@@ -5,7 +5,7 @@ public class Personaje_Martin {
 	public static void main(String[] args) {
 		//personaje//
 		System.out.print("Hoa mi nombre es José Joestar y tengo 26 años"
-				+ "mi fisico es bastante impresionante tengo una rara marca de nacimiento en forma de estrella en la prte trasera del hombro");
+				+ "mi fisico es bastante impresionante tengo una rara marca de nacimiento en forma de estrella en la parte trasera del hombro");
 
 	}
 
