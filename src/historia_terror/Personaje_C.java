d5871e8a3d3b65d61cd06845526159dc56d86919
zika2112 <24.damt.cesaralejandroramos@fundacionmontessori.com>
2024-12-08 17:36:13 +0100

    aporte a la historia y personaje

--- src/historia_terror/Personaje_C.java
+++ src/historia_terror/Personaje_C.java
@@ -7,13 +7,12 @@ public class Personaje_C {
 
 		System.out.println("Nombre: Jonny joestar");
 		System.out.println("Edad: 60 años");
-		System.out.println("Soy decendiente de una familia inglesa con una extraña marca de nacimiento con forma de estrela en nuestra espalda"
-				+"Tengo la meta de ser un caballero en toda la regla, siquiendo los valores del honorables de mis antepasados como un joestar\n"
-				+"Tengo una estatura de: 195 cm con fisico atletico para mi edad"
-				+"Tengo un hijo y la sospecha de un nieto perdido"
-				+"");
-		
-		
+		System.out.println("Soy decendiente de una familia inglesa "
+				+"Tengo la meta de ser un caballero en toda la regla, siguiendo los valores honorables de mis antepasados como un joestar\n"
+				+"Tengo una estatura de 195 cm , cabello medianamente largo peinado en direccion trasera \n"
+				+"de un color marron oscuro , con color de ojos marros oscuro ,con una extraña marca de nacimiento con forma de estrela en nuestra espalda \n");
+		System.out.println("Mi vestimenta esta conformada por una camiseta de la época, de tirantes sin mangas y de color azul \n"
+				+"pantalones largos de color azul y una botas altas marrones de cuero junto a unas hombreras de cuero para mi proteccion");
 		
 		
 	}
