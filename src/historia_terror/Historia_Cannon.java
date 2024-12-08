7f326c22c11987e06f928382118eddf724171a1d
dr <24.damt.danielrevuelto@fundacionmontessori.com>
2024-12-08 21:33:28 +0100

    hecho por daniel

--- src/historia_terror/Historia_Cannon.java
+++ src/historia_terror/Historia_Cannon.java
@@ -8,7 +8,7 @@ public class Historia_Cannon {
 		System.out.println("Narrador: \"Era una noche nevada en las afueras de Birmingham cuando los integrantes de la familia joestar sufrieron un accidente de carruaje donde el jinete pierde el control de las riendas, resultando en un caída en un lago congelado donde quedó atrapado un miembro de la familia junto a jotaru joestar y su madre .\"\r\n");
 		System.out.println("Narrador:”Durante el suceso y mientras el carruaje empezó a hundirse en el lago, de repente una extraña figura se acerca al lugar del accidente no con la intención de ayudar si no de sacar provecho de la circunstancia para sí mismo. ");
 		
-		System.out.println("!!: (Que suerte la mia, un accidente sin testigos, voy a ver que puedo sacar de aquí)");
+		System.out.println("!!:(Que suerte la mia, un accidente sin testigos, voy a ver que puedo sacar de aquí)");
 	}
 
 }
