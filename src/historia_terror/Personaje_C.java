94854013501e47fd75ebeee691402d9509661489
zika2112 <24.damt.cesaralejandroramos@fundacionmontessori.com>
2024-12-08 16:59:45 +0100

    correccion

--- src/historia_terror/Personaje_C.java
+++ src/historia_terror/Personaje_C.java
@@ -8,7 +8,7 @@ public class Personaje_C {
 		System.out.println("Nombre: Jonny joestar");
 		System.out.println("Edad: 60 años");
 		System.out.println("Soy decendiente de una familia inglesa con una extraña marca de nacimiento con forma de estrela en nuestra espalda"
-				+"Tengo la meta de ser un caballero en toda la regla, siquiendo los valores del honorables de mis antepasados como un joestar"
+				+"Tengo la meta de ser un caballero en toda la regla, siquiendo los valores del honorables de mis antepasados como un joestar\n"
 				+"Tengo una estatura de: 195 cm con fisico atletico para mi edad"
 				+"Tengo un hijo y la sospecha de un nieto perdido"
 				+"");
