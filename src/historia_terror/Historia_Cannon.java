5d6c7a7164e5caa8ecadc3788847a95053842119
martinlopeztrillo <22.sis.martinlopez@fundacionmontessori.com>
2024-12-08 21:25:22 +0100

    añadido de la historia Martin

--- src/historia_terror/Historia_Cannon.java
+++ src/historia_terror/Historia_Cannon.java
@@ -4,8 +4,9 @@ public class Historia_Cannon {
 
 	public static void main(String[] args) {
 
-		System.out.println("En el años 1880 en una familia de origen inglesa conformada por el padre, jotaru joestar y su hijo jose joestar");
-		System.out.println("...");
+		System.out.println("En el año 1880 en una familia de origen inglés conformada por el padre jotaru joestar y su padre jonny joestar, pasaría por un trágico momento que cambiaría sus vidas para siempre...\r\n");
+		System.out.println("Narrador: \"Era una noche nevada en las afueras de Birmingham cuando los integrantes de la familia joestar sufrieron un accidente de carruaje donde el jinete pierde el control de las riendas, resultando en un caída en un lago congelado donde quedó atrapado un miembro de la familia junto a jotaru joestar y su madre .\"\r\n");
+		System.out.println("Narrador:”Durante el suceso y mientras el carruaje empezó a hundirse en el lago, de repente una extraña figura se acerca al lugar del accidente no con la intención de ayudar si no de sacar provecho de la circunstancia para sí mismo. ");
 
 	}
 
