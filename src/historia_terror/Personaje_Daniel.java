9c4c05fe4e82926c9d8aa1c6da63d2731971375d
dr <24.damt.danielrevuelto@fundacionmontessori.com>
2024-12-08 18:33:48 +0100

    Mi personaje

--- src/historia_terror/Personaje_Daniel.java
+++ src/historia_terror/Personaje_Daniel.java
@@ -4,11 +4,11 @@ public class Personaje_Daniel {
 
 	public static void main(String[] args) {
 		System.out.println("Nombre: Jotaru Joestar");
-		System.out.println("Edad: 45");
+		System.out.println("Edad: 20");
 		System.out.println("Tengo una altura de 195cm, tanto mi pelo como mis ojos son de color negro y mi tono de piel es claro");
 		System.out.println(
 				"Tengo una rara marca de nacimiento en forma de estrella en la parte trasera de mi hombro izquierdo "
-						+ "y un buen físico para mi edad," + " creo que tengo un hijo en alguna parte de este mundo");
+						+ "y un buen físico para mi edad");
 		System.out.println("Frase típica: Yare Yare Daze");
 		System.out.println(
 				"Llevo puesto una camiseta verde, una gorra negra, una chaqueta negra que llega hasta mis rodillas, "
