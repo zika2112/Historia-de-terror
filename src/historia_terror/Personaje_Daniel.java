2f5cb44df625897fdc8922419855a2852cc37d56
dr <24.damt.danielrevuelto@fundacionmontessori.com>
2024-12-08 17:09:46 +0100

    Mi personaje

--- src/historia_terror/Personaje_Daniel.java
+++ src/historia_terror/Personaje_Daniel.java
@@ -8,6 +8,7 @@ public class Personaje_Daniel {
 		System.out.println("Tengo una rara marca de nacimiento en forma de estrella en la parte trasera de mi hombro izquierdo "
 				+ "y un buen físico para mi edad,"
 				+ " creo que tengo un hijo en alguna parte de este mundo");
+		System.out.println("Frase típica: Yare Yare Daze");
 
 	}
 
