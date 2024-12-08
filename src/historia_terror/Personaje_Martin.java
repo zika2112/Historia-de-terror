1243e85223498483e01d0ce4aaf97cf8089bbfb4
martinlopeztrillo <22.sis.martinlopez@fundacionmontessori.com>
2024-12-08 17:10:45 +0100

    aportacion al personaje

--- src/historia_terror/Personaje_Martin.java
+++ src/historia_terror/Personaje_Martin.java
@@ -7,6 +7,7 @@ public class Personaje_Martin {
 		System.out.print("Hoa mi nombre es José Joestar y tengo 26 años , tengo una estatura 1,93cm ");
 				System.out.println( "Tengo el pelo marron  con ojos color azules con un estilo de ropa de 1912 con un  fisico es impresionante tengo una rara marca de nacimiento en forma de estrella en la parte trasera del hombro");
 				System.out.println( "No se donde esta mi padre y si mi abuelo sigue vivo");
+				System.out.println("Mi frase tiica es: DIEGOO!!!!");
 
 	}
 
