3f2a866d51eb81a184d9f723ed62c259fd00fc06
zika2112 <24.damt.cesaralejandroramos@fundacionmontessori.com>
2024-12-09 16:37:40 +0100

    Update Historia_Cannon.java
    correccion de cesar

--- src/historia_terror/Historia_Cannon.java
+++ src/historia_terror/Historia_Cannon.java
@@ -7,12 +7,8 @@ public class Historia_Cannon {
 		System.out.println("En el año 1880 en una familia de origen inglés conformada por el padre jotaru joestar y su padre jonny joestar, pasaría por un trágico momento que cambiaría sus vidas para siempre...\r\n");
 		System.out.println("Narrador: \"Era una noche nevada en las afueras de Birmingham cuando los integrantes de la familia joestar sufrieron un accidente de carruaje donde el jinete pierde el control de las riendas, resultando en un caída en un lago congelado donde quedó atrapado un miembro de la familia junto a jotaru joestar y su madre .\"\r\n");
 		System.out.println("Narrador:”Durante el suceso y mientras el carruaje empezó a hundirse en el lago, de repente una extraña figura se acerca al lugar del accidente no con la intención de ayudar si no de sacar provecho de la circunstancia para sí mismo. ");
-<<<<<<< HEAD
-
-=======
 		
 		System.out.println("!!:(Que suerte la mia, un accidente sin testigos, voy a ver que puedo sacar de aquí)");
->>>>>>> 7f326c22c11987e06f928382118eddf724171a1d
 	}
 
 }
